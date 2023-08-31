2ac34c4e4a2b502652d09a3881e4166242b04a11
Andre Luiz <aluno@IPIRANGA.FATEC>
2023-08-31 18:06:21 -0300

    ap06(java) teste compilado

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,9 @@
+public class TesteLivroDeNotas{
+    static public void main (String args []){
+        //variavel de referencia
+        //malloc
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+        System.out.println("ate logo");
+    }
+}
